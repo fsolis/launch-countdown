809c2e4945709819be87ef4748630d3def8166da
Freddy <fsolis.se@gmail.com>
2021-06-08 18:34:43 -0700

    Added Scss and Initial Page Breakout

--- src/App.svelte
+++ src/App.svelte
@@ -1,25 +1,19 @@
 <script lang="ts">
-	export let name: string;
+	import Main from './components/Main.svelte'
+	import Footer from './components/Footer.svelte'
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<Main />
+	<Footer />
 </main>
 
-<style>
+<style type="text/scss">
 	main {
 		text-align: center;
-		padding: 1em;
+		padding: 1rem;
 		max-width: 240px;
 		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
 	}
 
 	@media (min-width: 640px) {
---
+++ src/components/Main.svelte
@@ -0,0 +1,12 @@
+<main>
+    <h1>We're launching soon</h1>
+
+  Days
+  Hours
+  Minutes
+  Seconds
+  
+</main>
+
+<style type="text/scss">
+</style>
